86d439ddfc086d865e62086f35868545eab0397d
BustDot <Bust.dev@outlook.com>
2023-08-18 22:19:04 +0800

    feat: remove redundant authenticate method

--- dauthz/backends/casbin_backend.py
+++ dauthz/backends/casbin_backend.py
@@ -1,34 +1,18 @@
 from django.contrib.auth import get_user_model
-from django.contrib.auth.backends import BaseBackend
+from django.contrib.auth.backends import ModelBackend
 
 from dauthz.core import enforcer, enforcers
-
 
 UserModel = get_user_model()
 
 
-class CasbinBackend(BaseBackend):
+class CasbinBackend(ModelBackend):
     """
     Check permissions with Casbin.
     """
 
     def __init__(self):
         self.enforcer = enforcer
-
-    def authenticate(self, request, username=None, password=None, **kwargs):
-        if username is None:
-            username = kwargs.get(UserModel.USERNAME_FIELD)
-        if username is None or password is None:
-            return
-        try:
-            user = UserModel._default_manager.get_by_natural_key(username)
-        except UserModel.DoesNotExist:
-            # Run the default password hasher once to reduce the timing
-            # difference between an existing and a nonexistent user (#20760).
-            UserModel().set_password(password)
-        else:
-            if user.check_password(password) and self.user_can_authenticate(user):
-                return user
 
     def user_can_authenticate(self, user):
         """
