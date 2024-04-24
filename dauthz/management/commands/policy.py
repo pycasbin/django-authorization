36a11f586f8723a69c3272162a698fd46f4c1e8d
Almas Baktubayev <a.baktubayev@gmail.com>
2024-04-24 20:27:17 +0500

    feat: code refactoring (#19)
    * fix: typo in docstring
    
    * refactor: deletion of an unnecessary variable
    
    * refactor: Use of f-string and slight code shortening/formatting
    
    * fix: refactor group.py for black linter rules
    
    * fix: restored old logic of message generation for Exception
    
    * fix: change logic for check None in enforcer_name variable

--- dauthz/management/commands/policy.py
+++ dauthz/management/commands/policy.py
@@ -19,21 +19,21 @@ class Command(BaseCommand):
         parser.add_argument("--enforcer", type=str, help="Name of Enforcer")
 
     def handle(self, *args, **options):
-        enforcer_name = options.get("enforcer")
-        if enforcer_name is None:
-            enforcer_name = "DEFAULT"
+        enforcer_name = options.get("enforcer") or "DEFAULT"
         handler_enforcer = None
 
         try:
             handler_enforcer = enforcers[enforcer_name]
         except KeyError:
-            raise Exception("Enforcer `" + enforcer_name + "` not found")
+            raise Exception(f"Enforcer `{enforcer_name}` not found")
 
         action = options.get("action")
         action = action.upper()
+
         sub = options.get("sub")
         obj = options.get("obj")
         act = options.get("act")
+
         if action == "ADD":
             res = handler_enforcer.add_policy(sub, obj, act)
             if res is True:
@@ -43,6 +43,6 @@ class Command(BaseCommand):
                 self.stderr.write("Policy not added success")
         elif action == "GET":
             res = handler_enforcer.enforce(sub, obj, act)
-            print("{}, {}, {} ---> {}".format(sub, obj, act, res))
+            print(f"{sub}, {obj}, {act} -> {res}")
         else:
             self.stderr.write("Action not found")
--- dauthz/utils.py
+++ dauthz/utils.py
@@ -3,10 +3,9 @@ import importlib
 
 def import_class(name):
     """Import class from string
-    e.g. `package.module.ClassToImport` returns the `ClasToImport` class"""
+    e.g. `package.module.ClassToImport` returns the `ClassToImport` class"""
     components = name.split(".")
     module_name = ".".join(components[:-1])
     class_name = components[-1]
     module = importlib.import_module(module_name)
-    class_ = getattr(module, class_name)
-    return class_
+    return getattr(module, class_name)
